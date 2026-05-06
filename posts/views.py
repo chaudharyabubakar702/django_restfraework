4c2fe07ae3b174ae00cea7907d61ab8b80ba13a6
Ch Abubakar <chaudharyabubakar702@gmail.com>
2026-05-06 13:01:21 +0500

    Permission classes

--- posts/views.py
+++ posts/views.py
@@ -1,60 +1,51 @@
-from django.db.models import QuerySet
 from rest_framework.request import Request
-from rest_framework.response import  Response
-from rest_framework import status, generics,mixins
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view,APIView
+from rest_framework.response import Response
+from rest_framework import status, generics, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from posts.models import Post
 from posts.serializers import PostSerializer
-from django.shortcuts import get_object_or_404
-
-
-# 📌 Dummy posts data
-
-
+from .permissions import AuthorOrReadOnly
 
 @api_view(http_method_names=['GET', 'POST'])
+@permission_classes([AllowAny])
 def homepage(request: Request):
-
     if request.method == 'POST':
-        serializer = PostSerializer(data=request.data,many=True)
-
+        serializer = PostSerializer(data=request.data)  # ✅ removed many=True
         if serializer.is_valid():
             serializer.save()
-
             return Response({
                 'message': 'Post created successfully',
                 'data': serializer.data
             }, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     return Response({'message': 'Hello World!'})
 
 
 class PostListCreateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
+
+     
 
     def get(self, request: Request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    def post(self, request: Request, *args, **kwargs):  # ← make sure this exists!
+    def post(self, request: Request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
 
-
-
-class PostRetrieveUpdateDeleteView(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = [AuthorOrReadOnly]
 
-    def get(self,request: Request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
+    def get(self, request: Request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self,request: Request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
+    def put(self, request: Request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self,request: Request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
+    def delete(self, request: Request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
